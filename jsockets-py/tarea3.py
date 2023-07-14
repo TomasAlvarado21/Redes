adf00679dd2acf43beca210fa7a9301554c29a52
Tomas Alvarado <55769898+TomasAlvarado21@users.noreply.github.com>
2023-07-13 23:58:01 -0400

    o

--- jsockets-py/tarea3.py
+++ jsockets-py/tarea3.py
@@ -1,13 +1,14 @@
+import socket, jsockets
 from scapy.all import *
 
 
 client_ip = "127.0.0.1"
-client_port = 65109
+client_port = 65122 #puerto del cliente, se debe modificar para que coincida con el puerto del cliente
 server_ip = "127.0.0.1"
-server_port = 1818
+server_port = 1818 
 message = "Paquete interceptado y modificado."
 
-conf.L3socket = L3RawSocket
+#conf.L3socket = L3RawSocket
 
 packet = IP(src=client_ip, dst=server_ip) / UDP(sport=client_port, dport=server_port) / Raw(load=message.encode())
    
